114b3fa0ba529641260130bf346fa2227952b7d0
double2kill <379563000@qq.com>
2020-06-02 13:16:54 +0800

    feat: 🎸 modify backend url

--- src/web/public/config.js
+++ src/web/public/config.js
@@ -1,5 +1,5 @@
 window.CONFIG = {
-  SEARCH_ORIGIN: 'http://table.greatwebtech.cn/search/',
+  SEARCH_ORIGIN: '/search/',
   // SEARCH_ORIGIN: backend url,
   LOAD_DATA_ENTER_PAGE: true,
   // LOAD_DATA_ENTER_PAGE: false,
